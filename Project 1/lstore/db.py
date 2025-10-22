59ac03d6d6a0fd24ef0fd202c49f1c192a0e7d53
Michal Klopotowski <123220580+MichalKlop@users.noreply.github.com>
2025-10-21 19:20:03 -0700

    db is a dict

--- Project 1/lstore/db.py
+++ Project 1/lstore/db.py
@@ -1,10 +1,10 @@
 from lstore.table import Table
 
-class Database():
+
+class Database:
 
     def __init__(self):
-        self.tables = []
-        pass
+        self.tables = {}  # Store tables by name
 
     # Not required for milestone1
     def open(self, path):
@@ -13,26 +13,26 @@ class Database():
     def close(self):
         pass
 
-    """
-    # Creates a new table
-    :param name: string         #Table name
-    :param num_columns: int     #Number of Columns: all columns are integer
-    :param key: int             #Index of table key in columns
-    """
     def create_table(self, name, num_columns, key_index):
+        """
+        Creates a new table
+
+        :param name: string         # Table name
+        :param num_columns: int     # Number of Columns: all columns are integer
+        :param key_index: int       # Index of table key in columns
+        """
         table = Table(name, num_columns, key_index)
+        self.tables[name] = table
         return table
 
-    
-    """
-    # Deletes the specified table
-    """
     def drop_table(self, name):
-        pass
+        """
+        Deletes the specified table
+        """
+        del self.tables[name]
 
-    
-    """
-    # Returns table with the passed name
-    """
     def get_table(self, name):
-        pass
+        """
+        Returns table with the passed name
+        """
+        return self.tables[name]
