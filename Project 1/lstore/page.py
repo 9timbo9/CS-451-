e9836fdf45967d450b7ae52b8c6ac3438d4de795
timbo9 <tntherussian@gmail.com>
2025-10-21 19:11:24 -0700

    page with print statements

--- Project 1/lstore/page.py
+++ Project 1/lstore/page.py
@@ -6,9 +6,15 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
+        print("checking capacity")
         pass
 
     def write(self, value):
+        print("writing value:", value)
         self.num_records += 1
+        pass
+    
+    def read(self, slot_number):
+        print("reading slot number:", slot_number)
         pass
 
